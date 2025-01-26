5a052175013b439006cc6efaa18e74fa6653cbea
Damian Czapnik <damian.whacky@proton.me>
2025-01-26 17:38:40 +0100

    initial commit

---
+++ src/age.ts
@@ -0,0 +1,7 @@
+var ageElement: HTMLElement = document.getElementById("age") as HTMLElement;
+setInterval(getPreciseAge, 500);
+
+function getPreciseAge(): void {
+  const age: number = (new Date().getTime() - new Date(2008, 4, 21).getTime()) / (1000 * 60 * 60 * 24 * 365.25);
+  ageElement.innerHTML = age.toFixed(8);
+}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+var headerElement: HTMLElement = document.querySelector("header") as HTMLElement;
+var footerElement: HTMLElement = document.querySelector("footer") as HTMLElement;
+
+headerElement.innerHTML = `
+    <div><a href="/">Home</a></div>
+    <div><a href="/contact/">Contact</a></div>
+    <div><a href="/blog/">Blog</a></div>
+`;
+
+footerElement.innerHTML = `
+    <div class="title">Honorable peeps:</div>
+
+    <div class="item" title="MY FUCKING SIS, LET'S FUCKING GET IT YO">
+      <a href="https://prpl.wtf/">
+        <p>Emma</p>
+      </a>
+    </div>
+
+    <div class="item" title="MY FUCKING G, MY BOY FOR FUCKING LIFE">
+      <a href="https://jswdev.nl/">
+        <p>JSW</p>
+      </a>
+    </div>
+
+    <div class="item" title="THE FUCKING DOLPHIN, MY FUCKING BOY">
+      <a href="https://geen-dolfijn.nl/">
+        <p>Definitely-Not-A-Dolphin</p>
+      </a>
+    </div>
+`;
